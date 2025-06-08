891e33ce6ae1fc60a6e63f564ffa7df6e0309f03
redllamas <sam.ellard@gmail.com>
2025-06-09 07:10:22 +0900

    new Odin Project project

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+end
